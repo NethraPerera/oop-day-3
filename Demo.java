2898071122ee012aec3e1f22233480ccca60b12b
NethraPerera <mr.esanda2001@gmail.com>
2026-06-28 09:23:04 +0530

    swing package java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
 class Demo{
     public static void main(String args []){
-        System.out.println("aaa");
+        JFrame f1=new JFrame("Calculator");
+        f1.setSize(400,400);
+        f1.setLocationRelativeTo(null);
+        f1.setDefaultCloseOperation(2);
+        f1.setVisible(true);
         
     }
 }
