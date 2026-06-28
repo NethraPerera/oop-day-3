a7b666e59e8f70a5f69480641587c41d2e1a35c5
NethraPerera <mr.esanda2001@gmail.com>
2026-06-28 10:37:21 +0530

    girdLayout

--- Demo.java
+++ Demo.java
@@ -1,33 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 class Demo{
-    public static void main(String args []){
-        JFrame f1=new JFrame("Calculator");
-        f1.setSize(400,400);
-        f1.setLocationRelativeTo(null);
-        f1.setDefaultCloseOperation(2);
-        f1.setLayout(new FlowLayout(FlowLayout.LEFT));
-        //0->FlowLayout.CENTER
-		//1->FlowLayout.RIGHT
-		//2->FlowLayout.LEFT
-
-        JButton b1=new  JButton("Add");
-        b1.setFont(new Font("",1,30));
-        f1.add(b1);
-        JButton b2=new  JButton("Delete");
-        b2.setFont(new Font("",1,30));
-        b2.setSize(300,400);
-        f1.add(b2);
-        JButton b3=new  JButton("Update");
-        b3.setFont(new Font("",1,30));
-        f1.add(b3);
-        JButton b4=new  JButton("View");
-        b4.setFont(new Font("",1,30));
-        f1.add(b4);
-        
-        
-        //f1.pack();
-        f1.setVisible(true);
-        
-    }
+	public static void main(String args[]){
+		JFrame f1=new JFrame("Calculator");	
+		f1.setSize(400,400);
+		f1.setLocationRelativeTo(null);
+		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		f1.setLayout(new GridLayout(4,4));
+		
+		JButton[] btArray=new JButton[16];
+		String[] buttonText={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"}; 
+		for (int i = 0; i < btArray.length; i++){
+			btArray[i]=new JButton(buttonText[i]);
+			btArray[i].setFont(new Font("",1,30));
+			f1.add(buttonText[i],btArray[i]);
+		}
+		f1.setVisible(true);
+	}
 }
