caaac97ee9a8227b5476f0fdb914cce98efb50a8
NethraPerera <mr.esanda2001@gmail.com>
2026-06-28 09:42:08 +0530

    JButton class

--- Demo.java
+++ Demo.java
@@ -1,10 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
 class Demo{
     public static void main(String args []){
         JFrame f1=new JFrame("Calculator");
         f1.setSize(400,400);
         f1.setLocationRelativeTo(null);
         f1.setDefaultCloseOperation(2);
+        
+        JButton b1=new  JButton("North");
+        b1.setText("North");
+        f1.add("North",b1);
+        JButton b2=new  JButton("South");
+        b2.setText("South");
+        f1.add("South",b2);
+        JButton b3=new  JButton("East");
+        b3.setText("East");
+        f1.add("East",b3);
+        JButton b4=new  JButton("West");
+        b4.setText("West");
+        f1.add("West",b4);
+        JButton b5=new  JButton("Center");
+        b5.setText("Center");
+        f1.add("Center",b5);
+        
+        //f1.pack();
         f1.setVisible(true);
         
     }
