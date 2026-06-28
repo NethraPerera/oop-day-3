55bd11f90090df6336b5fd29c5e042807d4e8ba4
NethraPerera <mr.esanda2001@gmail.com>
2026-06-28 11:31:07 +0530

    without inheriting

--- Demo.java
+++ Demo.java
@@ -1,30 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
-
+class Calculator{
+	JFrame f2;
+	Calculator(){}
+	Calculator(String name){
+		f2=new JFrame(name);
+	}
+	public void setSize(int length,int width){
+		f2.setSize(length,width);
+	} 
+	public void setLocationRelativeTo(Component location){
+		f2.setLocationRelativeTo(location);
+	}
+	public void setDefaultCloseOperation(int operation){
+		f2.setDefaultCloseOperation(operation);
+	}
+	public void setVisible(boolean isVisible){
+		f2.setVisible(isVisible);
+	}
+}
 class Demo{
 	public static void main(String args[]){
-		JFrame f1=new JFrame("Calculator");	
+		JFrame f1=new JFrame("JFrame");
 		f1.setSize(300,300);
 		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		//f1.setLayout(new GridLayout(4,4,3,3));
-		
-		JPanel centerPanel=new JPanel();
-		centerPanel.setLayout(new GridLayout(4,4,3,3));
-		
-		JButton[] btArray=new JButton[16];
-		String[] buttonText={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
-		for (int i = 0; i < 16; i++){
-			btArray[i]=new JButton(buttonText[i]);
-			btArray[i].setFont(new Font("",1,25));
-			centerPanel.add(btArray[i]);
-		}
-		
-		JTextField textField=new JTextField();
-		
-		f1.add("North",textField);
-		f1.add("Center",centerPanel);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f1.setVisible(true);
+		
+		Calculator c1=new Calculator("Calculator");
+		c1.setSize(300,300);
+		c1.setLocationRelativeTo(null);
+		c1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c1.setVisible(true);
 	}
 }
